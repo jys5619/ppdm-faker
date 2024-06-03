37b4836b7b91ffebb1fc946ef91c908280903a7e
LAPTOP-LBHE4FM6\Administrator <noriks@nate.com>
2024-06-03 22:43:23 +0900

    Query From List & Edit Link

--- src/entity/query-form.ts
+++ src/entity/query-form.ts
@@ -1,104 +1,127 @@
-import { fakerKO as faker } from "@faker-js/faker";
+import { fakerKO as faker } from '@faker-js/faker'
 
 interface IQueryForm {
-  title?: string;
-  favorites?: string;
-  description?: string;
+  title?: string
+  favorites?: string
+  description?: string
   inputList?: {
-    name?: string;
-    title?: string;
-    type?: string;
-    arrayData?: string;
-  }[];
+    name?: string
+    title?: string
+    type?: string
+    arrayData?: string
+  }[]
   sqlList?: {
-    title?: string;
-    description?: string;
-    sql?: string;
-  }[];
-  state?: string;
+    title?: string
+    description?: string
+    sql?: string
+  }[]
+  state?: string
 }
 
 export function QueryForm() {
-  let dataList = [];
-  let result: string[] = [];
+  let dataList = []
+  let result: string[] = []
 
   // MAIN
   let mainIQueryForms: IQueryForm[] = [
     {
-      title: "SQL1",
-      favorites: "#1 #2 #3",
-      description: "SQL1 Description",
+      title: 'DEPT, EMP 조회',
+      favorites: '#ACCOUNTING #RESEARCH #SALES #OPERATIONS',
+      description: 'DEPT, EMP 조회',
       inputList: [
         {
-          name: "v1",
-          title: "V1필드",
-          type: "INPUT",
-          arrayData: "",
-        },
-        {
-          name: "v2",
-          title: "V2필드",
-          type: "SELECT",
-          arrayData: "AA|BB|CC",
+          name: 'dname',
+          title: '부서명',
+          type: 'INPUT',
+          arrayData: '',
         },
       ],
       sqlList: [
         {
-          title: "쿼리문1",
-          description: "쿼리문1 설명",
-          sql: "SELECT :v1, :v2 FROM DUAL",
+          title: 'DEPT조회',
+          description: 'EMP 데이터 조회 DEPT: ACCOUNTING, RESEARCH, SALES, OPERATIONS',
+          sql: 'SELECT * FROM TB_DEPT WHERE DNAME = :dname',
         },
         {
-          title: "쿼리문2",
-          description: "쿼리문2 설명",
-          sql: "SELECT :v1, :v2 FROM DUAL",
+          title: 'EMP조회',
+          description: 'EMP 데이터 조회 JOB: PRESIDENT, MANAGER, SALESMAN, CLERK, ANALYST',
+          sql: 'SELECT * FROM TB_EMP WHERE DEPTNO IN (SELECT DEPTNO FROM TB_DEPT DNAME = :dname)',
         },
       ],
-      state: "Active",
+      state: 'Active',
     },
-  ];
+    {
+      title: 'EMP 조회',
+      favorites: '#ACCOUNTING,PRESIDENT #RESEARCH,PRESIDENT #SALES,MANAGER #OPERATIONS,ANALYST',
+      description: `EMP 조회
+DEPT: ACCOUNTING, RESEARCH, SALES, OPERATIONS
+JOB: PRESIDENT, MANAGER, SALESMAN, CLERK, ANALYST`,
+      inputList: [
+        {
+          name: 'dname',
+          title: '부서명',
+          type: 'INPUT',
+          arrayData: '',
+        },
+        {
+          name: 'job',
+          title: '직업',
+          type: 'SELECT',
+          arrayData: 'PRESIDENT|MANAGER|SALESMAN|CLERK|ANALYST',
+        },
+      ],
+      sqlList: [
+        {
+          title: 'EMP조회',
+          description: 'EMP조회 설명',
+          sql: 'SELECT * FROM TB_EMP WHERE DEPTNO IN (SELECT DEPTNO FROM TB_DEPT DNAME = :dname) AND JOB = :job',
+        },
+      ],
+      state: 'Active',
+    },
+  ]
 
-  mainIQueryForms = mainIQueryForms.map(m => {
-    return { ...m, id: faker.string.uuid() };
-  });
+  mainIQueryForms = mainIQueryForms.map((m) => {
+    return { ...m, id: faker.string.uuid() }
+  })
 
   for (const mainIQueryForm of mainIQueryForms) {
-    const id = faker.string.uuid();
-    const createdAt = faker.date.past().toISOString().slice(0, 19).replace("T", " ");
-    const updatedAt = faker.date.recent().toISOString().slice(0, 19).replace("T", " ");
-    dataList.push({ id, createdAt, updatedAt, ...mainIQueryForm });
+    const id = faker.string.uuid()
+    const createdAt = faker.date.past().toISOString().slice(0, 19).replace('T', ' ')
+    const updatedAt = faker.date.recent().toISOString().slice(0, 19).replace('T', ' ')
+    dataList.push({ id, createdAt, updatedAt, ...mainIQueryForm })
   }
 
-  dataList.forEach(data => {
+  dataList.forEach((data) => {
     result.push(
-      `INSERT INTO TB_QUERY_FORM (ID, TITLE, FAVORITES, DESCRIPTION, STATE, CREATED_AT, UPDATED_AT) VALUES ('${data.id}','${data.title}', '${data.favorites}', '${data.description}', '${data.state}', '${data.createdAt}', '${data.updatedAt}');`
-    );
+      `INSERT INTO TB_QUERY_FORM (ID, TITLE, FAVORITES, DESCRIPTION, STATE, CREATED_AT, UPDATED_AT) VALUES ('${data.id}','${data.title}', '${data.favorites}', '${data.description}', '${data.state}', '${data.createdAt}', '${data.updatedAt}');`,
+    )
 
     if (data.inputList && data.inputList.length > 0) {
-      data.inputList.forEach(input => {
+      data.inputList.forEach((input) => {
         result.push(
-          `INSERT INTO TB_QUERY_FORM_INPUT (ID, NAME, TITLE, TYPE,ARRAY_DATA, QUERY_FORM_ID CREATED_AT, UPDATED_AT) VALUES ('${faker.string.uuid()}','${
+          `INSERT INTO TB_QUERY_FORM_INPUT (ID, NAME, TITLE, TYPE,ARRAY_DATA, QUERY_FORM_ID, CREATED_AT, UPDATED_AT) VALUES ('${faker.string.uuid()}','${
             input.name
           }', '${input.title}', '${input.type}', '${input.arrayData}', '${data.id}', '${
             data.createdAt
-          }', '${data.updatedAt}');`
-        );
-      });
+          }', '${data.updatedAt}');`,
+        )
+      })
     }
 
     if (data.sqlList && data.sqlList.length > 0) {
-      data.sqlList.forEach(sql => {
+      data.sqlList.forEach((sql) => {
         result.push(
-          `INSERT INTO TB_QUERY_FORM_SQL (ID, TITLE, DESCRIPTION, SQL, QUERY_FORM_ID CREATED_AT, UPDATED_AT) VALUES ('${faker.string.uuid()}','${
+          `INSERT INTO TB_QUERY_FORM_SQL (ID, TITLE, DESCRIPTION, SQL, QUERY_FORM_ID, CREATED_AT, UPDATED_AT) VALUES ('${faker.string.uuid()}','${
             sql.title
           }','${sql.description}','${sql.sql}','${data.id}','${data.createdAt}', '${
             data.updatedAt
-          }');`
-        );
-      });
+          }');`,
+        )
+      })
     }
-  });
+  })
 
-  console.log("CREATE TB_QUERY_FORM".padEnd(80, "-") + `(${result.length})`);
-  return { dataList, result };
+  console.log('CREATE TB_QUERY_FORM'.padEnd(80, '-') + `(${result.length})`)
+  return { dataList, result }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,48 @@
-import { DbUtil } from "./db-util";
-import { Database } from "./entity/database";
-import { Menu } from "./entity/menu";
-import { MenuRole } from "./entity/menu-role";
-import { User } from "./entity/user";
-import { UserRole } from "./entity/user-role";
-import { promisify } from "util";
+import { DbUtil } from './db-util'
+import { Database } from './entity/database'
+import { Menu } from './entity/menu'
+import { MenuRole } from './entity/menu-role'
+import { QueryForm } from './entity/query-form'
+import { User } from './entity/user'
+import { UserRole } from './entity/user-role'
+import { promisify } from 'util'
 
 async function run() {
-  const sqlite3 = require("sqlite3").verbose();
-  const db = new sqlite3.Database("D:/dev/workspace/ppdm/ppdm-server/db/dev/ppdm.db");
-  const query = promisify(db.all).bind(db);
+  const sqlite3 = require('sqlite3').verbose()
+  const db = new sqlite3.Database('D:/dev/workspace/ppdm/ppdm-server/db/dev/ppdm.db')
+  const query = promisify(db.all).bind(db)
 
-  const sqlList: string[] = [];
-  sqlList.push("DELETE FROM TB_USER_ROLE");
-  sqlList.push("DELETE FROM TB_USER");
-  sqlList.push("DELETE FROM TB_MENU_ROLE");
-  sqlList.push("DELETE FROM TB_MENU");
-  console.log("DELETE TABLE".padEnd(80, "-") + `(${sqlList.length})`);
+  const sqlList: string[] = []
+  sqlList.push('DELETE FROM TB_USER_ROLE')
+  sqlList.push('DELETE FROM TB_USER')
+  sqlList.push('DELETE FROM TB_MENU_ROLE')
+  sqlList.push('DELETE FROM TB_MENU')
+  sqlList.push('DELETE FROM TB_QUERY_FORM')
+  sqlList.push('DELETE FROM TB_QUERY_FORM_INPUT')
+  sqlList.push('DELETE FROM TB_QUERY_FORM_SQL')
+  console.log('DELETE TABLE'.padEnd(80, '-') + `(${sqlList.length})`)
 
-  const userData = User(100);
-  sqlList.push(...userData.result);
+  const userData = User(100)
+  sqlList.push(...userData.result)
 
-  const userRoleData = UserRole(userData.users);
-  sqlList.push(...userRoleData.result);
+  const userRoleData = UserRole(userData.users)
+  sqlList.push(...userRoleData.result)
 
-  const menuData = Menu();
-  sqlList.push(...menuData.result);
+  const menuData = Menu()
+  sqlList.push(...menuData.result)
 
-  const menuRoleData = MenuRole(menuData.menus);
-  sqlList.push(...menuRoleData.result);
+  const menuRoleData = MenuRole(menuData.menus)
+  sqlList.push(...menuRoleData.result)
 
-  const databaseData = Database();
-  sqlList.push(...databaseData.result);
+  const databaseData = Database()
+  sqlList.push(...databaseData.result)
+
+  const queryFormData = QueryForm()
+  sqlList.push(...queryFormData.result)
 
   for (const sql of sqlList) {
-    await query(sql);
+    await query(sql)
   }
 }
 
-run();
+run()
